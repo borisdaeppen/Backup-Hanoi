5780762551e465f8e5dbfdb242d54875d6f55024
Boris Däppen <bdaeppen.perl@gmail.com>
2018-05-05 22:37:53 +0200

    added POD and dist.ini - ready for CPAN?

--- lib/Backup/Hanoi.pm
+++ lib/Backup/Hanoi.pm
@@ -1,4 +1,5 @@
 package Backup::Hanoi;
+# ABSTRACT: select backup according to algo
 
 use strict;
 use warnings;
@@ -72,3 +73,34 @@ sub _get_right_zeros_from_digital_representation {
 }
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 SYNOPSIS
+
+This is an early release.
+This code is currently not used in production by the author.
+Use it with care!
+
+ my @devices = ('A', 'B', 'C', 'D');
+ my $backup  = Backup::Hanoi->new(\@devices);
+
+ # calculate the next 100 backup cycles
+ for (0 .. 99) {
+     print $backup->get_device_for_cycle($_);
+     print "\n";
+ }
+
+See also the script L<backup-hanoi>.
+
+=head1 FUNCTIONS
+
+=head2 get_device_for_cycle
+
+Takes a reference to a list with at least three items.
+
+=head2 get_device_for_cycle
+
+Give a positive integer greather than zero, receive a string which reprecents the selcted item.
